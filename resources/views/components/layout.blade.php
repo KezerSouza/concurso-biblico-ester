0cd70ee1d9216c7315a0992e0909421fc9fa7c4d
Kezer Souza <kezer@discente.ufg.br>
2026-04-18 02:31:23 -0300

    Favicon da igreja

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name') }}</title>
+    <link rel="icon" type="image/png" href="/favicon.png">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
